7e281fab65972a5886a3ef1e30b1162188efb750
youngmumi <k0419kyj1@gmail.com>
2025-03-19 12:53:37 +0900

    first update

---
+++ code/2402_08.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int main() {
+	char s[15] = "ADCDBCCAADBDBAB";
+	char *a = "DB";
+	char *b = "AD";
+
+	int i, c, ac = 0, bc = 0;
+
+	for (i = 0; i < 15; i++) {
+		for (c = 0; c < 2; c++) {
+			if (s[i + c] != a[c]) break;
+		}
+		if (c == 2) ac++;
+		for (c = 0;c < 2;c++) {
+			if (s[i + c] != b[c]) break;
+		}
+		if (c == 2)bc++;
+	}
+	printf("%d %d", ac, bc);
+	return 0;
+}
